d79fc0bf30e64e848939c97479b5db931e906ea4
Beoran <beoran@rubyforge.org>
2010-03-08 14:21:35 +0100

    Reducing the amount of files used due to cgo bug.

--- sdl/sdl.go
+++ sdl/sdl.go
@@ -55,6 +55,55 @@ func (self mystring) cstr() (* C.char) {
   return C.CString(string(self))
 }
 
+// converts ints to bools
+func i2b(res int) (bool) {
+  if res != 0 { return true } 
+  return false
+}
+
+// CPUINFO
+
+// This function returns true if the CPU has the RDTSC instruction
+func HasRDTRSC() (bool) {
+  return i2b(int(C.SDL_HasRDTSC()))
+}
+
+// This function returns true if the CPU has MMX features
+func HasMMX() (bool) {
+  return i2b(int(C.SDL_HasMMX()))
+}
+ 
+
+// This function returns true if the CPU has MMX Ext. features
+func HasMMXExt() (bool) {
+  return i2b(int(C.SDL_HasMMXExt()))
+}
+ 
+// This function returns true if the CPU has 3DNow features
+func Has3DNow() (bool) {
+  return i2b(int(C.SDL_Has3DNow()))
+}
+
+// This function returns true if the CPU has 3DNow Ext. features
+func Has3DNowExt() (bool) {
+  return i2b(int(C.SDL_Has3DNowExt()))
+}
+
+// This function returns true if the CPU has SSE features
+func HasSSE() (bool) {
+  return i2b(int(C.SDL_HasSSE()))
+}
+ 
+// This function returns true if the CPU has SSE2 features
+func HasSSE2() (bool) {
+  return i2b(int(C.SDL_HasSSE2()))
+}
+
+// This function returns true if the CPU has AltiVec features
+func HasAltiVec() (bool) {
+  return i2b(int(C.SDL_HasAltiVec()))
+}
+
 
 // The available application states
 const (
